069d9eb07f4fc710dea5012c96f9f6b9bf482737
JEFTE VITOR DANTAS SILVA <20251101110010@ifrn.local>
2025-11-14 07:37:34 -0300

    função dos filmes

---
+++ CineData.py
@@ -0,0 +1,19 @@
+filmes_fav = {}
+def coloca_filme():
+    q = int(input("Quantos filmes deseja adicionar aos favoritos?"))
+    for i in range(q):
+        filme = str(input("Nome do filme:"))
+        ano = int(input("Ano de lançamento:"))
+        genero = str(input("Genero:"))
+        filmes_fav["filme"] = filme
+        filmes_fav["ano"] = ano
+        filmes_fav["genero"] = genero
+        return filmes_fav
+
+print("Olá, bem vindo ao CineData!/n")
+adicionar = ("Deseja adicionar filmes a sua lista de favoritos? (s/n)").lower()
+
+if adicionar == "s":
+    coloca_filme()
+    
+    
